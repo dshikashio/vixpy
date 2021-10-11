2c4706b07f7c07ca43dde40ec8120b12414e5c07
Doug S <doug.shikashio@gmail.com>
2021-10-11 00:07:36 -0400

    updates

--- osx-build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python setup.py build
-install_name_tool -change libvixAllProducts.dylib "/Applications/VMware Fusion.app/Contents/Public/libvixAllProducts.dylib" $(find . -name _vixpy.so)
-sudo python setup.py install
-
-sudo rm -rf build dist vixpy.egg-info
